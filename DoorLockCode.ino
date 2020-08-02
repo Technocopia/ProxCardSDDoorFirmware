eb96f7da792eba234ddc1902421e865f7d8bd1f0
Kevin Harrington <mad.hephaestus@gmail.com>
2020-08-02 19:41:30 -0400

    loading from file strange

--- DoorLockCode.ino
+++ DoorLockCode.ino
@@ -7,7 +7,14 @@
 #define button 32
 #include <Arduino.h>
 #include "codes.h" // also where #define sitecode is
-//static unsigned long int cards[] = { 0, 0, 0, 0 };
+#define NUM_STATIC_CARDS (sizeof(cards) / sizeof(unsigned long int))
+#include <ArduinoJson.h>
+#include <SD.h>
+#include <SPI.h>
+#include <FS.h>
+#define numCards 300
+// Configuration that we'll store on disk
+
 #define sitecode 17
 
 char bits[80];
@@ -16,8 +23,17 @@ unsigned long long bitw = 0;
 unsigned int timeout = 1000;
 boolean valid = true;
 int parity(unsigned long int x);
+const char *filename = "/db.json";  // <- SD library uses 8.3 filenames
+const size_t CAPACITY = JSON_ARRAY_SIZE(300);
+// allocate the memory for the document
+StaticJsonDocument<CAPACITY> doc_read;
+StaticJsonDocument<CAPACITY> doc_write;
+JsonArray array_read = doc_read.to<JsonArray>();
+JsonArray array_write = doc_write.to<JsonArray>();
 //portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 // Wiegand 0 bit ISR. Triggered by wiegand 0 wire.
+bool loadCardMode = false;
+long startLoadCardMode = 0;
 void IRAM_ATTR W0ISR() {
 	if (digitalRead(W0))
 		return;
@@ -41,13 +57,76 @@ void IRAM_ATTR W1ISR() {
 
 }
 
+// Prints the content of a file to the Serial
+void printFile(const char *filename) {
+	// Open file for reading
+	File file = (File) SD.open(filename);
+	if (!file) {
+		Serial.println(F("Failed to read file"));
+		return;
+	}
+
+	// Extract each characters by one by one
+	while (file.available()) {
+		Serial.print((char) file.read());
+	}
+	Serial.println();
+
+	// Close the file (File's destructor doesn't close the file)
+	file.close();
+}
+
 void setup() {
 	// put your setup code here, to run once:
 	Serial.begin(115200);
+	// create an empty array
+
+	// Initialize SD library
+	while (!SD.begin()) {
+		Serial.println(F("Failed to initialize SD library"));
+		delay(1000);
+	}
+	File fileTest = (File) SD.open(filename, FILE_WRITE);
+	if (!fileTest) {
+		Serial.println(F("Database missing"));
+	}
+	fileTest.close();
+
+	DeserializationError err;
+	do {
+		File f_l = (File) SD.open(filename, FILE_READ);
+//		// parse a JSON array
+		err = deserializeJson(doc_read, f_l);
+		f_l.close();
+		Serial.println("deserialize result " + String(err.c_str()));
+
+		if (err != DeserializationError::Ok) {
+			File 	filew = SD.open(filename, FILE_WRITE);
+			Serial.println("Load default values..." + String(NUM_STATIC_CARDS));
+			// Populate database with default values
+			for (int i = 0; i < NUM_STATIC_CARDS; i++) {
+				array_write.add(cards[i]);
+			}
+			// serialize the array and send the result to Serial
+			serializeJson(doc_write, filew);
+			filew.flush();
+			filew.close();
+			//ESP.restart();
+		} else {
+			Serial.println("Number of cards " + String(array_read.size()));
+//			for (JsonVariant v : array_read) {
+//				Serial.println("Val = " + String(v.as<int>()));
+//			}
+		}
+	} while (err != DeserializationError::Ok);
+	// Dump config file
+	Serial.println(F("Print config file..."));
+	printFile(filename);
 
 	pinMode(W0, INPUT_PULLUP);
 	pinMode(W1, INPUT_PULLUP);
 	pinMode(button, INPUT_PULLUP);
+	pinMode(0, INPUT_PULLUP);
 	pinMode(DoorP, OUTPUT);
 	pinMode(DoorE, OUTPUT);
 	digitalWrite(DoorP, LOW);
@@ -92,88 +171,113 @@ void open() {
 	digitalWrite(DoorE, LOW);
 	delay(200);
 	digitalWrite(DoorE, HIGH);
-	digitalWrite(DoorP, LOW);// close
+	digitalWrite(DoorP, LOW); // close
 
 	Serial.println("Locking door");
 }
 
-void IRAM_ATTR loop() {
-	if (!digitalRead(button)) {
-		open();
-		return;
-	}
+bool haveCard() {
+	return (((millis() - timeout) > 500) && bitcnt > 30);
+}
 
-	//portEXIT_CRITICAL(&mux);
+long int getIDOfCurrentCard() {
 	unsigned long long bitwtmp = bitw;
 	int bitcnttmp = bitcnt;
+	bitcnt = 0;
+	bitw = 0;
 	//portEXIT_CRITICAL(&mux);
-	if (((millis() - timeout) > 500) && bitcnttmp > 30) { // The reader hasn't sent a bit in 2000 units of time. Process card.
-	//Serial.print((long unsigned int)(bitw>>32),BIN);
-	//Serial.print((long unsigned int)bitw,BIN);
-	//  bitw = 0x122D9F628;
-	//portEXIT_CRITICAL(&mux);
-		bitcnt = 0;
-		bitw = 0;
-		//portEXIT_CRITICAL(&mux);
-		for (int i = bitcnttmp; i != 0; i--)
-			Serial.print((unsigned int) (bitwtmp >> (i - 1) & 0x00000001)); // Print the card number in binary
-		Serial.print(" (");
-		Serial.print(bitcnttmp);
-		Serial.println(")");
-		boolean ep, op;
-		unsigned int site;
-		unsigned long int card;
+	for (int i = bitcnttmp; i != 0; i--)
+		Serial.print((unsigned int) (bitwtmp >> (i - 1) & 0x00000001)); // Print the card number in binary
+	Serial.print(" (");
+	Serial.print(bitcnttmp);
+	Serial.println(")");
+	boolean ep, op;
+	unsigned int site;
+	unsigned long int card;
 
-		// Pick apart card.
-		site = (bitwtmp >> 25) & 0x00007f;
-		card = (bitwtmp >> 1) & 0xffffff;
-		op = (bitwtmp >> 0) & 0x000001;
-		ep = (bitwtmp >> 32) & 0x000001;
+	// Pick apart card.
+	site = (bitwtmp >> 25) & 0x00007f;
+	card = (bitwtmp >> 1) & 0xffffff;
+	op = (bitwtmp >> 0) & 0x000001;
+	ep = (bitwtmp >> 32) & 0x000001;
 
-		// Check parity
-		if (parity(site) != ep)
-			valid = false;
-		if (parity(card) == op)
-			valid = false;
+	// Check parity
+	if (parity(site) != ep)
+		valid = false;
+	if (parity(card) == op)
+		valid = false;
 
-		// Print card info
-		Serial.print("Got " + String());
-		Serial.print("Site: ");
-		Serial.println(site);
-		Serial.print("Card: ");
-		Serial.println(card);
-		Serial.print("ep: ");
-		Serial.print(parity(site));
-		Serial.println(ep);
-		Serial.print("op: ");
-		Serial.print(parity(card));
-		Serial.println(op);
-		Serial.print("Parity Check: ");
-		Serial.println(valid ? "Valid" : "Error");
+	// Print card info
+	Serial.print("Got " + String());
+	Serial.print("Site: ");
+	Serial.println(site);
+	Serial.print("Card: ");
+	Serial.println(card);
+	Serial.print("ep: ");
+	Serial.print(parity(site));
+	Serial.println(ep);
+	Serial.print("op: ");
+	Serial.print(parity(card));
+	Serial.println(op);
+	Serial.print("Parity Check: ");
+	Serial.println(valid ? "Valid" : "Error");
 
-		valid = true;
+	valid = true;
+	return card;
+}
 
-		for (int i = 0; i < sizeof(cards) / sizeof(unsigned long int); i++) {
+void IRAM_ATTR loop() {
+	if (!digitalRead(button) && !haveCard()) {
+		open();
+		if (haveCard()) {
+
+		}
+		return;
+	}
+	if (!digitalRead(0) && loadCardMode==false) {
+		// load card mode
+		loadCardMode = true;
+		startLoadCardMode = millis();
+		Serial.println("Start load card mode");
+	}
+	if (loadCardMode && (millis() - startLoadCardMode) > 5000) {
+		loadCardMode = false;
+		Serial.println("End load Card Mode, writing");
+		// serialize the array and send the result to Serial
+		File file = (File) SD.open(filename, FILE_WRITE);
+		serializeJson(doc_write, file);
+		file.flush();
+		file.close();
+		printFile(filename);
+	}
+	if (haveCard()) { // The reader hasn't sent a bit in 2000 units of time. Process card.
+		unsigned long int card = getIDOfCurrentCard();
+		for (JsonVariant v : array_read) {
 			//Serial.println("Checking "+String(cards[i]));
-			if (cards[i] == card) { // Is it in the DB?
+			if (v.as<int>() == card) { // Is it in the DB?
 				Serial.println(
 						"\t\tMatch! " + String(card) + " to card form list "
-								+ String(cards[i]));
+								+ String(v.as<int>()));
 				open();
 				return;
 			}
 		}
-		Serial.println("Error! " + String(card));
-		//digitalWrite(DoorE, HIGH);
-		digitalWrite(DoorE, LOW);
-		delay(500);
-		digitalWrite(DoorE, HIGH);
-		delay(500);
-		digitalWrite(DoorE, LOW);
-		delay(500);
-		digitalWrite(DoorE, HIGH);
-		delay(500);
-
+		if (loadCardMode) {
+			Serial.println("Adding card! "+String(card));
+			array_write.add(card);
+			array_read.add(card);
+			startLoadCardMode = millis();
+		} else {
+			Serial.println("Error! " + String(card));
+			//digitalWrite(DoorE, HIGH);
+			digitalWrite(DoorE, LOW);
+			delay(500);
+			digitalWrite(DoorE, HIGH);
+			delay(500);
+			digitalWrite(DoorE, LOW);
+			delay(500);
+			digitalWrite(DoorE, HIGH);
+		}
 	} else {
 		delay(30);
 	}
